da2236255e92b0d6f96b8a15106781b0d1a0bfe3
Alvaro Alonso <airuritac@gmail.com>
2023-01-09 15:28:03 +0100

    starting

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const connection = require("./database/connection");
 const dotenv = require("dotenv");
 const app = require("./src/server");
 
+const port = 4000;
 
 // Config dotenv
 dotenv.config();
@@ -10,7 +11,6 @@ dotenv.config();
 // Conexion a la DB
 connection();
 
-const port = 4000;
 
 
 
