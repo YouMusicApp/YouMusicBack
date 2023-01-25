c1c1e3194078f4981bce5f321a829df9771c869f
lidiettes <hello@lidiettes.com>
2023-01-25 16:21:32 +0100

    fix error with tech lead

--- src/controller/artists.controller.js
+++ src/controller/artists.controller.js
@@ -4,57 +4,57 @@ const getAllArtists = (req, res) => {
     const artists = Artist.find({})
 
     artists.exec((error, data) => {
-        if (error || !data) {
+        if (error || null) {
             return res.status(404).json({
                 status: "error",
-                mensaje: "Hay un error, o no se ha encontrado ninguna cancion"
+                mensaje: "Artists not found"
             })
         }
         return res.status(200).json({
             status: "success",
             info: data,
-            mensaje: "Las caciones estan disponibles en .tracks!"
+            mensaje: "The songs are available in tracks"
         })
     })
 }
+// db.Student.find().lean().exec()
 
 const createArtist = async (req, res) => {
+    const { body } = req;
     try {
-
-        const { body } = req;
-
-        const artist = new Artist({...body });
-
+        const artist = new Artist({ ...body });
         await artist.save();
-        res.status(201).json({ message: 'Artista creado exitosamente', data: artist });
+        res.status(201).json({ message: 'Successfully created artist', data: artist });
     } catch (error) {
         res.status(400).json({ mensaje: error.message });
     }
 };
 
 const editArtist = async (req, res) => {
+    const { body, params: { id } } = req;
     try {
-        const { name, genres, views, thumbnail } = req.body;
-        const artist = await Artist.findById(req.params.id);
-        if (!artist) res.status(404).json({ message: 'Artista no encontrado' });
-        artist.name = name || artist.name;
-        artist.genres = genres || artist.genres;
-        artist.views = views || artist.views;
-        artist.thumbnail = thumbnail || artist.thumbnail;
-        await artist.save();
-        res.json({ message: 'Artista actualizado exitosamente' });
+
+        const artist = await Artist.findByIdAndUpdate({ _id: id }, { ...body });
+        if (!artist) {
+            res.status(404).json({ message: 'Artist not found' })
+        } else {
+            res.json({ message: 'Successfully updated artist', data: artist });
+        }
     } catch (error) {
-        res.status(500).json({ message: 'Error al actualizar el artista', error });
+        res.status(400).json({ mensaje: error.message });
     }
 };
 
 const deleteArtist = async (req, res) => {
     try {
         const artist = await Artist.findByIdAndRemove(req.params.id);
-        if (!artist) res.status(404).json({ message: 'Artista no encontrado' });
-        res.json({ message: 'Artista eliminado exitosamente' });
+        if (!artist) {
+            res.status(404).json({ message: 'Artist not found' })
+        } else {
+            res.json({ message: 'Artist deleted' });
+        }
     } catch (error) {
-        res.status(500).json({ message: 'Error al eliminar el artista', error });
+        res.status(400).json({ mensaje: error.message });
     }
 };
 
