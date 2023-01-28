bb1fb954412c1cf6da2b24938d29c114c982803c
lidiettes <hello@lidiettes.com>
2023-01-28 18:56:25 +0100

    refact

--- src/controller/users.controllers.js
+++ src/controller/users.controllers.js
@@ -1,64 +1,38 @@
 // Import 
 const User = require("../models/users.model");
 
-const checkUser = async (req, res) => {
-    try {
-        const params = req.params
-        const data = await User.find({ email: params.email })
-        if (data.length) {
+const checkUser = (req, res) => {
+    const params = req.params
+    
+    User.find({ email: params.email }, (error, data) => {
+        if (error || !data) {
+            return res.status(500).json({
+                status: "error",
+                mensaje: "Error al buscar"
+            })
+        }
+        if (!!data.length) {
+            console.log('El usuario existe en la ddbb');
             return res.status(200).json({
                 status: "success",
                 info: data,
-                message: "User exists in the database"
+                mensaje: "En la consola esta la lista pisha!!"
             })
         } else {
+            console.log('El usuario no existe en la ddbb');
             return res.status(200).json({
                 status: "success",
                 info: false,
-                message: "User does not exist in the database"
+                mensaje: "Ese email no esta en la bbdd"
             })
         }
-    } catch (error) {
-        return res.status(500).json({
-            status: "error",
-            message: "Error searching for user"
-        })
-    }
+    })
 }
 
-
-// const checkUser = (req, res) => {
-//     const params = req.params
-//     User.find({ email: params.email }, (error, data) => {
-//         if (error || !data) {
-//             return res.status(500).json({
-//                 status: "error",
-//                 mensaje: "Error al buscar"
-//             })
-//         }
-//         if (!!data.length) {
-//             console.log('El usuario existe en la ddbb');
-//             return res.status(200).json({
-//                 status: "success",
-//                 info: data,
-//                 mensaje: "En la consola esta la lista pisha!!"
-//             })
-//         } else {
-//             console.log('El usuario no existe en la ddbb');
-//             return res.status(200).json({
-//                 status: "success",
-//                 info: false,
-//                 mensaje: "Ese email no esta en la bbdd"
-//             })
-//         }
-//     })
-// }
-
-
-const createUser = async (req, res) => {
+const createUser = (req, res) => {
     try {
-        // recogemos los params del body y se asigna como constante
-        const params = req.body;
+        // recogemos los params del body
+        let params = req.body;
 
         // validamos datos (con la funcion que tenemos en la carpeta helper)
 
@@ -66,56 +40,28 @@ const createUser = async (req, res) => {
         const $user = new User(params)
 
         // guardar el articulo en la ddbb
-        const data = await $user.save()
-
-        //devolver el post
-        return res.status(200).json({
-            status: "success",
-            info: data,
-            message: "User created successfully"
+        $user.save((error, data) => {
+            if (error || !data) {
+                return res.status(400).json({
+                    status: "error",
+                    mensaje: "No se ha guardado el post"
+                })
+            }
+            //devolver el post
+            return res.status(200).json({
+                status: "success",
+                info: data,
+                mensaje: "El post ha sido guardado"
+            })
         })
     } catch (error) {
+        console.log(error);
         return res.status(400).json({
             status: "error",
-            message: error.message
+            mensaje: "No se ha guardado el post"
         })
     }
 }
-
-
-// const createUser = (req, res) => {
-//     try {
-//         // recogemos los params del body
-//         let params = req.body;
-
-//         // validamos datos (con la funcion que tenemos en la carpeta helper)
-
-//         // Creamos el objeto a guardar
-//         const $user = new User(params)
-
-//         // guardar el articulo en la ddbb
-//         $user.save((error, data) => {
-//             if (error || !data) {
-//                 return res.status(400).json({
-//                     status: "error",
-//                     mensaje: "No se ha guardado el post"
-//                 })
-//             }
-//             //devolver el post
-//             return res.status(200).json({
-//                 status: "success",
-//                 info: data,
-//                 mensaje: "El post ha sido guardado"
-//             })
-//         })
-//     } catch (error) {
-//         console.log(error);
-//         return res.status(400).json({
-//             status: "error",
-//             mensaje: "No se ha guardado el post"
-//         })
-//     }
-// }
 
 const editUser = (req, res) => {
     let idUser = req.params.id;
