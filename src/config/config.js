7f073f8dfdf4c1dad6511f49fcecd85b9789ac72
Juan Carlos M Giner <jcmginer@gmail.com>
2023-01-30 16:13:35 +0100

    Middleware done

--- src/config/config.js
+++ src/config/config.js
@@ -1,50 +1,50 @@
-// const dotenv = require('dotenv')
-// const logger = require('loglevel')
+const dotenv = require('dotenv')
+const logger = require('loglevel')
 
 // //REPASAR, LOGGER NO INSTALADO
 
-// dotenv.config()
+dotenv.config()
 
-// logger.enableAll()
+logger.enableAll()
 
-// const ENV = process.env.NODE_ENV || 'development'
+const ENV = process.env.NODE_ENV || 'development'
 
-// const CONFIG = {
-//     development: {
-//         app: {
-//             PORT: process.env.PORT || 4000
-//         },
-//         logger: {
-//             warn: logger.warn,
-//             info: logger.info,
-//             error: logger.error,
-//             trace: logger.trace,
-//             debug: logger.debug
-//         },
-//         db: {
-//             uri: process.env.MONGODB_URL
-//         },
-//         auth0: {
-//             client_origin: process.env.APP_ORIGIN,
-//             audience: process.env.AUTH0_AUDIENCE,
-//             issuer: process.env.AUTH0_ISSUER
-//         }
-//     },
-//     production: {
-//         app: {
-//             PORT: process.env.PORT || 4000
-//         },
-//         logger: {
-//             warn: logger.warn,
-//             info: logger.info,
-//             error: logger.error,
-//             trace: logger.trace,
-//             debug: logger.debug
-//         },
-//         db: {
-//             url: 'http://localhost:4000'
-//         }
-//     }
-// }
+const CONFIG = {
+    development: {
+        app: {
+            PORT: process.env.PORT || 4000
+        },
+                logger: {
+                    warn: logger.warn,
+                    info: logger.info,
+                    error: logger.error,
+                    trace: logger.trace,
+                    debug: logger.debug
+                },
+                db: {
+                    uri: process.env.MONGODB_URL
+                },
+        auth0: {
+            client_origin: process.env.APP_ORIGIN,
+            audience: process.env.AUTH0_AUDIENCE,
+            issuer: process.env.AUTH0_ISSUER
+        }
+            },
+            production: {
+                app: {
+                    PORT: process.env.PORT || 4000
+                },
+                logger: {
+                    warn: logger.warn,
+                    info: logger.info,
+                    error: logger.error,
+                    trace: logger.trace,
+                    debug: logger.debug
+                },
+                db: {
+                    url: 'http://localhost:4000'
+                }
+    }
+}
 
-// module.exports = CONFIG[ENV]
+module.exports = CONFIG[ENV]
