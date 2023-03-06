fa288ae23581a09f3a08f9efa8eb046ef62c94e8
lidiettes <hello@lidiettes.com>
2023-03-06 22:26:35 +0100

    deploy3

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -9,7 +9,7 @@ router.get('/checkuser/:email', userController.checkUser)
 
 router.post('/createuser', userController.createUser)
 
-router.patch('/edituser/:id', checkJwt, userController.editUser)
+// router.patch('/edituser/:id', checkJwt, userController.editUser)
 // router.patch('/edituserplaylist/:id', checkJwt, userController.editUser)
 
 
