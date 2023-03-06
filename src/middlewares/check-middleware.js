58f3c2bdab006387d9b6d011ffee2dcdc0b857af
lidiettes <hello@lidiettes.com>
2023-03-06 21:50:28 +0100

    without-middleware

--- src/middlewares/check-middleware.js
+++ src/middlewares/check-middleware.js
@@ -1,20 +1,20 @@
-const { expressjwt: jwt } = require('express-jwt')
-const jwksRsa = require('jwks-rsa')
-const { getConfig } = require('../config/config')
+// const { expressjwt: jwt } = require('express-jwt')
+// const jwksRsa = require('jwks-rsa')
+// const { getConfig } = require('../config/config')
 
-const checkJwt = () => jwt({
-    secret: jwksRsa.expressJwtSecret({
-        cache: true,
-        rateLimit: true,
-        jwksRequestsPerMinute: 5,
-        jwksUri: `${getConfig().auth0.issuer}.well-known/jwks.json`
-    }),
+// const checkJwt = () => jwt({
+//     secret: jwksRsa.expressJwtSecret({
+//         cache: true,
+//         rateLimit: true,
+//         jwksRequestsPerMinute: 5,
+//         jwksUri: `${getConfig().auth0.issuer}.well-known/jwks.json`
+//     }),
 
-    audience: getConfig().auth0.audience,
-    issuer: getConfig().auth0.issuer,
-    algorithms: ['RS256']
-})
+//     audience: getConfig().auth0.audience,
+//     issuer: getConfig().auth0.issuer,
+//     algorithms: ['RS256']
+// })
 
-module.exports = {
-    checkJwt
-}
+// module.exports = {
+//     checkJwt
+// }
