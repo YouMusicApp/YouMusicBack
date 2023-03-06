de8d917dfa7941c17048d944eb7858bac7562be0
lidiettes <hello@lidiettes.com>
2023-03-06 21:46:33 +0100

    deploy back

--- src/middlewares/check-middleware.js
+++ src/middlewares/check-middleware.js
@@ -1,17 +1,17 @@
-const { auth0 } = require('../config/config')
 const { expressjwt: jwt } = require('express-jwt')
 const jwksRsa = require('jwks-rsa')
+const { getConfig } = require('../config/config')
 
-const checkJwt = jwt({
+const checkJwt = () => jwt({
     secret: jwksRsa.expressJwtSecret({
         cache: true,
         rateLimit: true,
         jwksRequestsPerMinute: 5,
-        jwksUri: `${auth0.issuer}.well-known/jwks.json`
+        jwksUri: `${getConfig().auth0.issuer}.well-known/jwks.json`
     }),
 
-    audience: auth0.audience,
-    issuer: auth0.issuer,
+    audience: getConfig().auth0.audience,
+    issuer: getConfig().auth0.issuer,
     algorithms: ['RS256']
 })
 
